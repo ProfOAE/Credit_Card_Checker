1fb78c6c68d912c4732e2648fa14ee078ce91e5e
ProfOAE <oheneadjeie@gmail.com>
2021-10-16 17:57:32 +0000

    Create Credit Card Checker.js
    checks the validity of a credit card

---
+++ Credit Card Checker.js
@@ -0,0 +1,131 @@
+// All valid credit card numbers
+const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
+
+// All invalid credit card numbers
+const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
+const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
+const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
+const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
+const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
+
+// Can be either valid or invalid
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
+
+// An array of all the arrays above
+const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
+
+
+// Add your functions below:
+
+const validateCred = (arr) => {
+  /* adding the last element to sum
+
+  let lastElement = arr.length - 1;
+  let sum = arr[lastElement];
+  // removing the last element 
+  let checkArray = arr;
+  checkArray.pop(); 
+  for (let i = 0; i < checkArray.length; i++){
+   if (i % 2 === 0){ 
+    checkArray[i] = checkArray[i] * 2;
+   }
+    if (checkArray[i] > 9){
+      checkArray[i] = checkArray[i] - 9
+    }
+    sum = sum + checkArray[i]; */
+
+    let sum = 0;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    let value = arr[i]
+    if ((arr.length - 1 - i) % 2 === 1) {
+      value *= 2;
+      if (value > 9) {
+        value = value - 9;
+      }
+    }
+    sum = sum + value;
+  }
+
+  return sum % 10 === 0;
+  }
+   
+   /*if (sum % 10 === 0){
+     return true;
+   } return false;*/
+
+/*Using Tenary Statement
+sum % 10 === 0? true : false;
+
+  return sum % 10 === 0;
+  
+};*/
+
+// Test functions:
+console.log(validateCred(valid1)); // Should print true
+console.log(validateCred(invalid1)); // Should print false
+
+// a function which contains nested array to return invalid arrays
+const findInvalidCards = (array) => {
+  let invalidArray = [];
+ for (let i = 0; i < array.length; i++){
+  let ans = validateCred();
+  if ( ans === false){
+    invalidArray.push(array[i])
+ } 
+  }
+return invalidArray;
+};
+console.log();
+// Test function
+/*console.log(findInvalidCards([valid1, valid2, valid3, valid4, valid5]));// Shouldn't print anything
+console.log(findInvalidCards([invalid1, invalid2, invalid3, invalid4, invalid5])); // Should print all of the numbers*/
+
+const idInvalidCardCompanies = (invalidOnes) => {
+  const invalidToCompanies = [];
+
+  for (let i = 0; i < invalidOnes.length; i++){
+    switch (invalidOnes[i][0]){
+      case 3:
+      if (invalidToCompanies.indexOf('Amex') === -1){
+        invalidToCompanies.push('Amex')
+      }
+      break;
+      case 4:
+      if (invalidToCompanies.indexOf('Visa') === -1){
+        invalidToCompanies.push('visa')
+      }
+      break;
+      case 5:
+      if (invalidToCompanies.indexOf('Mastercard') === -1){
+        invalidToCompanies.push('Mastercard')
+      }
+      break;
+      case 6:
+      if (invalidToCompanies.indexOf('Discover') === -1){
+        invalidToCompanies.push('Discover')
+      }
+      break;
+      default:
+      console.log('Company not found');
+      break;
+      
+    }
+  }
+
+  return invalidToCompanies;
+}
+
+
+
+
+
+
+
